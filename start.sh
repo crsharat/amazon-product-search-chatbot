c2d4356a17ec07fe23b9533128bd3d06bf066bb0
cr.sharat <sharat@inzyte.com>
2016-06-22 19:48:16 +0530

    -y added

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo apt-get update
-sudo apt-get install libxml2-dev libxslt-dev zlib1g-dev python2.7-dev
+sudo apt-get install -y libxml2-dev libxslt-dev zlib1g-dev python2.7-dev
 sudo pip install lxml
 sudo pip install requests
 chmod +x parser.py
